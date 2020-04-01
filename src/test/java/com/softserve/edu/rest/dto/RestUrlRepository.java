d92001af62c087d832fce27c5c23fe36d0a37df3
Pavlo Tytarchuk <tytarchuk.pavel@gmail.com>
2020-04-01 18:18:28 +0300

    added get userItems method to RestUrlRepository class

--- src/test/java/com/softserve/edu/rest/dto/RestUrlRepository.java
+++ src/test/java/com/softserve/edu/rest/dto/RestUrlRepository.java
@@ -1,10 +1,10 @@
 package com.softserve.edu.rest.dto;
 
 public final class RestUrlRepository {
-	private static String server = "http://localhost:8080";
+    private static String server = "http://localhost:8080";
 
-	private RestUrlRepository() {
-	}
+    private RestUrlRepository() {
+    }
 
 //	public static RestUrl getAdminAuthorized() {
 //		return new RestUrl()
@@ -16,82 +16,91 @@ public final class RestUrlRepository {
 //		// .addDeleteUrl("/logout");
 //	}
 
-	public static RestUrl getUserAuthorized() {
-		return new RestUrl()
-				.addBaseUrl(server)
-				.addGetUrl("/login/users").addGetUrl("/login/admins") // getLoginedUsers, getLoginedAdmins
-				.addPostUrl("/login").addPostUrl("/logout") // login, logout
-				.addPutUrl("") // TODO Change Password
-				.addDeleteUrl("") // run POST
-				.addPatchUrl(""); // TODO Change Password
-		// .addDeleteUrl("/logout");
-	}
+    public static RestUrl getUserAuthorized() {
+        return new RestUrl()
+                .addBaseUrl(server)
+                .addGetUrl("/login/users").addGetUrl("/login/admins") // getLoginedUsers, getLoginedAdmins
+                .addPostUrl("/login").addPostUrl("/logout") // login, logout
+                .addPutUrl("") // TODO Change Password
+                .addDeleteUrl("") // run POST
+                .addPatchUrl(""); // TODO Change Password
+        // .addDeleteUrl("/logout");
+    }
 
-	public static RestUrl getApplication() {
-		return new RestUrl()
-				.addBaseUrl(server)
-				.addGetUrl("/reset")
-				.addPostUrl("")
-				.addPutUrl("")
-				.addDeleteUrl("")
-				.addPatchUrl("");
-	}
+    public static RestUrl getApplication() {
+        return new RestUrl()
+                .addBaseUrl(server)
+                .addGetUrl("/reset")
+                .addPostUrl("")
+                .addPutUrl("")
+                .addDeleteUrl("")
+                .addPatchUrl("");
+    }
 
-	public static RestUrl getTokenLifetime() {
-		return new RestUrl()
-				.addBaseUrl(server)
-				.addGetUrl("/tokenlifetime")
-				.addPostUrl("")
-				.addPutUrl("/tokenlifetime")
-				.addDeleteUrl("")
-				.addPatchUrl("");
-	}
+    public static RestUrl getTokenLifetime() {
+        return new RestUrl()
+                .addBaseUrl(server)
+                .addGetUrl("/tokenlifetime")
+                .addPostUrl("")
+                .addPutUrl("/tokenlifetime")
+                .addDeleteUrl("")
+                .addPatchUrl("");
+    }
 
-	public static RestUrl getUserItem() {
-		return new RestUrl()
-				.addBaseUrl(server)
-				.addGetUrl("/item/{index}/user/{name}")
-				.addPostUrl("")
-				.addPutUrl("")
-				.addDeleteUrl("")
-				.addPatchUrl("");
-	}
+    public static RestUrl getUserItem() {
+        return new RestUrl()
+                .addBaseUrl(server)
+                .addGetUrl("/item/{index}/user/{name}")
+                .addPostUrl("")
+                .addPutUrl("")
+                .addDeleteUrl("")
+                .addPatchUrl("");
+    }
 
-	public static RestUrl getItem() {
-		return new RestUrl().addBaseUrl(server)
-				.addGetUrl("/item/{index}")
-				.addPostUrl("/item/{index}")
-				.addPutUrl("/item/{index}")
-				.addDeleteUrl("/item/{index}")
-				.addPatchUrl("");
-	}
+    public static RestUrl getUserItems() {
+        return new RestUrl()
+                .addBaseUrl(server)
+                .addGetUrl("/item/user/{name}")
+                .addPostUrl("")
+                .addPutUrl("")
+                .addDeleteUrl("");
+    }
 
-	public static RestUrl getItems() {
-		return new RestUrl().addBaseUrl(server)
-				.addGetUrl("/items")
-				.addPostUrl("")
-				.addPutUrl("")
-				.addDeleteUrl("")
-				.addPatchUrl("");
-	}
+    public static RestUrl getItem() {
+        return new RestUrl().addBaseUrl(server)
+                .addGetUrl("/item/{index}")
+                .addPostUrl("/item/{index}")
+                .addPutUrl("/item/{index}")
+                .addDeleteUrl("/item/{index}")
+                .addPatchUrl("");
+    }
 
-	public static RestUrl getUsers() {
-		return new RestUrl()
-				.addBaseUrl(server)
-				.addGetUrl("/users")
-				.addPostUrl("")
-				.addPutUrl("")
-				.addDeleteUrl("")
-				.addPatchUrl("");
-	}
+    public static RestUrl getItems() {
+        return new RestUrl().addBaseUrl(server)
+                .addGetUrl("/items")
+                .addPostUrl("")
+                .addPutUrl("")
+                .addDeleteUrl("")
+                .addPatchUrl("");
+    }
 
-	public static RestUrl getCoolDownTime() {
-		return new RestUrl()
-				.addBaseUrl(server)
-				.addGetUrl("/cooldowntime")
-				.addPostUrl("")
-				.addPutUrl("/cooldowntime")
-				.addDeleteUrl("")
-				.addPatchUrl("");
-	}
+    public static RestUrl getUsers() {
+        return new RestUrl()
+                .addBaseUrl(server)
+                .addGetUrl("/users")
+                .addPostUrl("")
+                .addPutUrl("")
+                .addDeleteUrl("")
+                .addPatchUrl("");
+    }
+
+    public static RestUrl getCoolDownTime() {
+        return new RestUrl()
+                .addBaseUrl(server)
+                .addGetUrl("/cooldowntime")
+                .addPostUrl("")
+                .addPutUrl("/cooldowntime")
+                .addDeleteUrl("")
+                .addPatchUrl("");
+    }
 }
