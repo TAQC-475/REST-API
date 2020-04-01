c8fc96b999cea621422e4de1fdf2d56b92fab590
Pavlo Tytarchuk <tytarchuk.pavel@gmail.com>
2020-04-01 21:13:24 +0300

    Created UserItemsResource class and VerifyAdminCanGetUserItems test

--- src/test/java/com/softserve/edu/rest/data/UserRepository.java
+++ src/test/java/com/softserve/edu/rest/data/UserRepository.java
@@ -2,22 +2,23 @@ package com.softserve.edu.rest.data;
 
 public final class UserRepository {
 
-	private UserRepository() {
-	}
+    private UserRepository() {
+    }
 
-	public static User getDefault() {
-		return getAdmin();
-	}
+    public static User getDefault() {
+        return getAdmin();
+    }
 
-	public static User getAdmin() {
-		return new User("admin", "qwerty", true).addItem("My data");
-	}
-	public static User getValidUser() {
-		return new User("otlumtc", "qwerty", false);
-	}
+    public static User getAdmin() {
+        return new User("admin", "qwerty", true).addItem("My data");
+    }
 
-	public static User notExistingUser() {
-		return new User("Anna", "ksjddlfkjddqwerty", false).addItem("My data");
-	}
+    public static User getValidUser() {
+        return new User("otlumtc", "qwerty", false);
+    }
+
+    public static User notExistingUser() {
+        return new User("Anna", "ksjddlfkjddqwerty", false).addItem("My data");
+    }
 
 }
---
+++ src/test/java/com/softserve/edu/rest/resources/UserItemsResource.java
@@ -0,0 +1,17 @@
+package com.softserve.edu.rest.resources;
+
+import com.softserve.edu.rest.dto.RestUrlRepository;
+import com.softserve.edu.rest.engine.RestQueries;
+import com.softserve.edu.rest.entity.SimpleEntity;
+
+public class UserItemsResource extends RestQueries<SimpleEntity, SimpleEntity, SimpleEntity, SimpleEntity, SimpleEntity> {
+
+    protected UserItemsResource() {
+        super(RestUrlRepository.getUserItems(),
+                SimpleEntity.class,
+                SimpleEntity.class,
+                SimpleEntity.class,
+                SimpleEntity.class,
+                SimpleEntity.class);
+    }
+}
