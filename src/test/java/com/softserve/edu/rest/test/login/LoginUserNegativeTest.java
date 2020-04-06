a274dde2b715cebbce74b0de38e110fd3f72cfe5
BorisTitarenko <borys.tytarenko.pz.2017@lpnu.ua>
2020-04-06 16:05:31 +0300

    Add two negative tests

--- src/test/java/com/softserve/edu/rest/test/login/LoginUserNegativeTest.java
+++ src/test/java/com/softserve/edu/rest/test/login/LoginUserNegativeTest.java
@@ -1,13 +1,31 @@
 package com.softserve.edu.rest.test.login;
 
 import com.softserve.edu.rest.data.User;
+import com.softserve.edu.rest.services.GuestService;
+import com.softserve.edu.rest.services.LogginedUsersService;
 import com.softserve.edu.rest.test.RestTestRunner;
+import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class LoginUserNegativeTest extends RestTestRunner {
     @Test(dataProviderClass = UsersTestData.class, dataProvider = "nonExistingUserDataProvider")
     public void loginUserNegativeTest(User admin, User notExisingUser){
+        LogginedUsersService logginedUsersService = new GuestService()
+                .gotoLoginService()
+                .unsuccessfulUserLogin(notExisingUser)
+                .successfulAdminLogin(admin)
+                .gotoLogginedUsersService();
+        Assert.assertFalse(logginedUsersService.getLoggedUsers().contains(notExisingUser));
+    }
 
+    @Test(dataProviderClass = UsersTestData.class, dataProvider = "nonExistingAdminDataProvider")
+    public void loginAdminNegativeTest(User admin, User notExisingAdmin){
+        LogginedUsersService logginedUsersService = new GuestService()
+                .gotoLoginService()
+                .unsuccessfulUserLogin(notExisingAdmin)
+                .successfulAdminLogin(admin)
+                .gotoLogginedUsersService();
+        Assert.assertFalse(logginedUsersService.getLoggedUsers().contains(notExisingAdmin));
     }
 }
