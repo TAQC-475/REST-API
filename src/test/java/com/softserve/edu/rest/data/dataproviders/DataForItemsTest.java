92c4295e42f475e4e2b8efbded93b4d1a607107e
Pavlo Tytarchuk <tytarchuk.pavel@gmail.com>
2020-04-09 20:14:56 +0300

    added data provider classes for Items and Index tests

---
+++ src/test/java/com/softserve/edu/rest/data/dataproviders/DataForItemsTest.java
@@ -0,0 +1,27 @@
+package com.softserve.edu.rest.data.dataproviders;
+
+import com.softserve.edu.rest.data.ItemRepository;
+import com.softserve.edu.rest.data.UserRepository;
+import org.testng.annotations.DataProvider;
+
+public class DataForItemsTest {
+    @DataProvider
+    public Object[][] dataForGettingAllItemsTest() {
+        return new Object[][]{{UserRepository.getValidUser(), ItemRepository.getCoreI5(), ItemRepository.getCoreI7(), ItemRepository.getTestItemsList()}};
+    }
+
+    @DataProvider
+    public Object[][] dataForAdminGettingUserItemsTest() {
+        return new Object[][]{{UserRepository.getAdmin(), UserRepository.getValidUser()}};
+    }
+
+    @DataProvider
+    public Object[][] dataForVerifyingUserCantGetAdminItems() {
+        return new Object[][]{{UserRepository.getAdmin(), UserRepository.getValidUser(), ItemRepository.getCoreI7()}};
+    }
+
+    @DataProvider
+    public Object[][] dataForVerifyingUserCanGetAllItemsIndexes() {
+        return new Object[][]{{UserRepository.getValidUser(), ItemRepository.getCoreI5(), ItemRepository.getCoreI7(), ItemRepository.getTestItemsIndexes()}};
+    }
+}
--- src/test/java/com/softserve/edu/rest/test/items/IndexTest.java
+++ src/test/java/com/softserve/edu/rest/test/items/IndexTest.java
@@ -1,32 +1,15 @@
 package com.softserve.edu.rest.test.items;
 
 import com.softserve.edu.rest.data.Item;
-import com.softserve.edu.rest.data.ItemRepository;
 import com.softserve.edu.rest.data.User;
-import com.softserve.edu.rest.data.UserRepository;
+import com.softserve.edu.rest.data.dataproviders.DataForIndexTest;
 import com.softserve.edu.rest.services.ItemsService;
 import com.softserve.edu.rest.services.LoginService;
 import org.testng.Assert;
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class IndexTest {
-    @DataProvider
-    public Object[][] dataForCreatingItemWithValidIndexValue() {
-        return new Object[][]{{UserRepository.getValidUser(), ItemRepository.getItemWithMinIndex()},
-                {UserRepository.getValidUser(), ItemRepository.getItemWithIndexBetweenMinAndMax()},
-                {UserRepository.getValidUser(), ItemRepository.getItemWithMaxIndex()}};
-    }
-
-    @DataProvider
-    public Object[][] dataForCreatingItemWithInvalidIndexValue() {
-        return new Object[][]{{UserRepository.getValidUser(), ItemRepository.getItemWithLetterIndex()},
-                {UserRepository.getValidUser(), ItemRepository.getItemWithNegativeIndex()},
-                {UserRepository.getValidUser(), ItemRepository.getItemWithIndexHigherThanMax()},
-                {UserRepository.getValidUser(), ItemRepository.getItemWithSpecialCharacterIndex()}};
-    }
-
-    @Test(dataProvider = "dataForCreatingItemWithValidIndexValue")
+    @Test(dataProvider = "dataForCreatingItemWithValidIndexValue", dataProviderClass = DataForIndexTest.class)
     public void verifyCreatingItemWIthValidIndex(User user, Item itemWithValidIndex) {
         ItemsService itemsService = new LoginService()
                 .successfulUserLogin(user)
@@ -37,7 +20,7 @@ public class IndexTest {
         Assert.assertTrue(itemsService.getAllItemsList().contains(itemWithValidIndex));
     }
 
-    @Test(dataProvider = "dataForCreatingItemWithInvalidIndexValue")
+    @Test(dataProvider = "dataForCreatingItemWithInvalidIndexValue", dataProviderClass = DataForIndexTest.class)
     public void verifyItemCantBeCreatedWithInvalidIndex(User user, Item itemWithInvalidIndex) {
         String statusCode = new LoginService()
                 .successfulUserLogin(user)
